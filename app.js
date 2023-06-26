7b371231088292f9bf49271c46af2a05434bdbb3
Shankar Jadhav <shankar.jadhav@gaia.biz4solutions.com>
2023-06-27 01:57:52 +0530

    episode-2

--- app.js
+++ app.js
@@ -1,21 +1,18 @@
 
-
+import React from "react"
+import ReactDOM from "react-dom/client";
 
 
 const parent = React.createElement(
     "div",
-    {id: "parent"},
+    { id: "parent" },
     React.createElement(
         "div",
-        {id: "child"},
-        [React.createElement("h1",{},"I'm an h1 tag"),
-        React.createElement("h2",{},"I'm an h1 tag")]
+        { id: "child" },
+        [React.createElement("h1", {}, "I'm an h1 tag"),
+        React.createElement("h2", {}, "I'm an h1 tag")]
     )
 )
-
-
-
-
 
 // const heading = React.createElement(
 //     "h1",
