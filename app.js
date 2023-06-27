667724287528de41e18860f35f5a6f3c6d1fee74
Shankar Jadhav <shankar.jadhav@gaia.biz4solutions.com>
2023-06-28 01:04:18 +0530

    episode-03- react Fragment

--- app.js
+++ app.js
@@ -1,27 +1,34 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-
+const elem = <span>React Element</span>;
 
 // React Component
 // Class Based Component -DLB
 // Functional Component - NEW
 
-
-const Title = () => (
-   <h1 className="head" tabIndex="5">
-     Namaste React using JSX
-   </h1>
+const Title = (
+  <h1 className="head" tabIndex="5">
+    {elem}
+    Namaste React using JSX
+  </h1>
 );
 
+const number = 1000;
 
-//component composition -- component inside
+// React Fragment - behave like an empty tag
 const HeadingComponent1 = () => (
+  <>
     <div id="container">
-        <Title />
-        <h1 className="heading">Namaste React Functional Component</h1>
+      {Title}
+      <h1 className="heading">Namaste React Functional Component</h1>
     </div>
-)
+    <div id="container">
+      {Title}
+      <h1 className="heading">Namaste React Functional Component</h1>
+    </div>
+  </>
+);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
