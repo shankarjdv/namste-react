07e3224e01e20cfffaa95fe4719cd1ff5487ff88
Shankar Jadhav <shankar.jadhav@gaia.biz4solutions.com>
2023-06-28 00:39:56 +0530

    episode-03

--- app.js
+++ app.js
@@ -1,12 +1,28 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-// React Element
-const jsxHeading = <h1 className="head">Namaste React using JSX</h1>;
-console.log(jsxHeading);
 
 
+// React Component
+// Class Based Component -DLB
+// Functional Component - NEW
+
+
+const Title = () => (
+   <h1 className="head" tabIndex="5">
+     Namaste React using JSX
+   </h1>
+);
+
+
+//component composition -- component inside component
+const HeadingComponent1 = () => (
+    <div id="container">
+        <Title />
+        <h1 className="heading">Namaste React Functional Component</h1>
+    </div>
+)
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(jsxHeading);
+root.render(<HeadingComponent1 />);
