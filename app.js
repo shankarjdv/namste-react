f98143a0ba6114e419bab2e6ac12f1e29d44f22d
Shankar Jadhav <shankar.jadhav@gaia.biz4solutions.com>
2023-06-28 00:19:56 +0530

    episode-03

--- app.js
+++ app.js
@@ -2,15 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 // React Element
-
-const heading = React.createElement("h1", { id: "heading" }, "namste reaact");
-console.log(heading);
-
-// JSX - is not HTML in JS(it is xml/html like syntax)
-//JSX(transpiled before it reaches the JS) - PARCEL - Babel
-// JSX => React.createElement => ReactElement-Js Object => HTMLElement(render)
 const jsxHeading = <h1 className="head">Namaste React using JSX</h1>;
 console.log(jsxHeading);
+
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
